ca34a73259bbffce8d1558c844119aef51c10b73
truong93540 <truong93540@gmai.com>
2025-01-23 13:10:12 +0700

    Config

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./redux/slides/counterSlice";
+import styled from "styled-components";
+
+function App() {
+    const count = useSelector((state) => state.counter.value);
+    const dispatch = useDispatch();
+
+    const Button = styled.button`
+        color: grey;
+    `;
+
+    return (
+        <div>
+            <div>
+                <Button
+                    aria-label="Increment value"
+                    onClick={() => dispatch(increment())}>
+                    Increment
+                </Button>
+                <span>{count}</span>
+                <button
+                    aria-label="Decrement value"
+                    onClick={() => dispatch(decrement())}>
+                    Decrement
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default App;
